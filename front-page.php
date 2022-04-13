26f73d512da2b04584c05122bf35b11cb090bdb9
GodRyosuke <ienvs0828@outlook.jp>
2022-04-13 14:42:19 +0900

    initial wp change

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OCU KDEL Takubo Laboratory</title>
+    <title>OMU KDEL Takubo Laboratory</title>
     <script src="https://kit.fontawesome.com/29d47546b5.js" crossorigin="anonymous"></script>
     <!-- <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> -->
     <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script> 
@@ -14,7 +14,7 @@
 </head>
 <body>
     <header>
-        <a href="index.html" class="logo">OCU<span>KDEL</span></a>
+        <a href="index.html" class="logo">OMU<span>KDEL</span></a>
         <!-- <div class="menuToggle" onclick="toggleMenu();">
         </div> -->
         <i class="fa fa-bars"></i>
