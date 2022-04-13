a9c79bda33aed9f812d8231bfffb4cf9450c2e7f
GodRyosuke <ienvs0828@outlook.jp>
2022-04-13 11:43:52 +0900

    blogのhtmlはひとまずできた？

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,35 @@ function hideMenu() {
     // navLinks.style.display = "none";
 }
 
+// blog pageのside bar
+// $( document ).ready(function() {
+//     var $sticky = $('.sticky');
+//     var $stickyrStopper = $('.sticky-stopper');
+//     if (!!$sticky.offset()) { // make sure ".sticky" element exists
+  
+//         var generalSidebarHeight = $sticky.innerHeight();
+//         var stickyTop = $sticky.offset().top;
+//         var stickOffset = 0;
+//         var stickyStopperPosition = $stickyrStopper.offset().top;
+//         var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
+//         var diff = stopPoint + stickOffset;
+    
+//         $(window).scroll(function(){ // scroll event
+//             var windowTop = $(window).scrollTop(); // returns number
+    
+//             if (stopPoint < windowTop) {
+//                 $sticky.css({ position: 'absolute', top: diff });
+//             } else if (stickyTop < windowTop+stickOffset) {
+//                 $sticky.css({ position: 'fixed', top: stickOffset });
+//             } else {
+//                 $sticky.css({position: 'absolute', top: 'initial'});
+//             }
+//         });
+  
+//     }
+// });
+
+
 // while (1) {
 //     let UserInput = window.prompt("パスワードを入力して下さい:","");
 //     if( /\W+/g.test(UserInput) ) {
