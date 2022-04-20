f97a3dec46a779a464fefd9e5030efd9a47ed365
GodRyosuke <ienvs0828@outlook.jp>
2022-04-20 11:59:15 +0900

    sf

--- wp-content/themes/OcuKdelWeb/header.php
+++ wp-content/themes/OcuKdelWeb/header.php
@@ -13,6 +13,7 @@
   src="https://code.jquery.com/jquery-3.6.0.min.js"
   integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
   crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js" defer></script>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets//css/tomorrow-night.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.css">
