0f406361c31a0cd5a18923c3d90947ddd564d295
GodRyosuke <ienvs0828@outlook.jp>
2023-01-02 08:25:36 +0900

    dockerで開発できるようにした

---
+++ OcuKdelWeb/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+    
+    <section class="sub-header" id="sub-header">
+        <div class="content">
+            <h2>404 ERROR</h2>
+        </div>
+    </section>
+
+    <section class="content-404">
+        <h1>ERROR The page you search doesn't exist.</h1>
+    </section>
+    
+<?php get_footer(); ?>
---
+++ OcuKdelWeb/page-member.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+    
+    <section class="sub-header" id="sub-header">
+        <div class="content">
+            <h2>Member</h2>
+        </div>
+    </section>
+
+    <section class="member">
+        <img src="./assets/img/cafeteria.png" alt="">
+        <table>
+            <tr>
+                <th>Grade</th> <th>Name</th>
+            </tr>
+            <tr>
+                <td>Professor</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Postdoctoral Scholar</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Doctor</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Doctor</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Graduate</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Graduate</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Undergraduate</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Undergraduate</td> <td>Mitsuhide Akechi</td>
+            </tr>
+            <tr>
+                <td>Undergraduate</td> <td>Mitsuhide Akechi</td>
+            </tr>
+        </table>
+    </section>    
+
+<?php get_footer(); ?>
